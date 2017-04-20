264c743335b2c1e30faf6180fa3fba63aa0b42a1
Ryan W Quinlan <rryanq@caen-vnc-vm17.engin.umich.edu>
2017-04-19 21:54:56 -0400

    Added some stylistic changes, realized that there is a problem in change_bills that needs to be addressed later

--- dollar_bill_v2.cpp
+++ dollar_bill_v2.cpp
@@ -2,8 +2,9 @@
 Parameters to pass into main: 
   - Denominations of bills to try
   - Dollar amount to sum to
-
   - compile using g++ -std=c++11 dollar_bill_v2.cpp -o dollar_bill_v2.exe
+  - To run: ./dollar_bill_v2.exe [bill denominations] [amount to sum to]
+  - Ex: ./dollar_bill_v2.exe 100 50 20 10 5 2 1 100
 */
 
 #include <iostream>
@@ -13,6 +14,7 @@ Parameters to pass into main:
 #include <set>
 using namespace std;
 
+//custom comparator for dollars_in set
 struct Greater {
   template<class T>
   bool operator()(T const &a, T const &b) const { 
@@ -20,8 +22,7 @@ struct Greater {
   }
 };
 
-//REQUIRES: you are using all possible combinations of
-//1, 2, 5, 10, 20 and 50 dollar bills only. No 100 or anything else
+//REQUIRES: dollars_in is not empty
 //MODIFIES: dollars
 //EFFECTS: Counts the number of ways that dollar_amount can be
 //exceeded without using a bill that must be immediately returned
@@ -39,16 +40,14 @@ pair<int, int> count_ways(vector<int> dollars, int min_bill,
 bool check_for_solution(int change, const vector<int> &dollars, 
                         const vector<int> &change_bills);
 
-//REQUIRES: you are using all possible combinations of
-//1, 2, 5, 10, 20 and 50 dollar bills only. No 100 or anything else
+//REQUIRES: dollars_in is not empty
 //MODIFIES: change_bills
 //EFFECTS: uses change to find the simplest combination of
 //bills to return to the customer as change
 void update_change_bills(int change, vector<int> &change_bills, 
                          const set<int, Greater> &dollars_in);
 
-//REQUIRES: you are using all possible combinations of
-//1, 2, 5, 10, 20 and 50 dollar bills only. No 100 or anything else
+//REQUIRES: dollars_in is not empty
 //MODIFIES: dollars, denomination
 //EFFECTS: sets up dollars for next time and updates 
 //the denomination to add
@@ -60,8 +59,6 @@ void update_dollars(vector<int> &dollars, int &denomination,
 void print_solutions(vector<int> dollars, int max_bill, int dollar_amount,
 		     const set<int, Greater> &dollars_in);
 
-//in case I want to sort a vector from greatest to least
-
 
 //main takes ints that represent dollar denominations as its first arguments,
 //and the final argument is the dollar amount that should be summed to
@@ -70,16 +67,9 @@ int main(int argc, char * argv[]) {
   for(int i = 1; i < argc - 1; ++i) {
     dollars_in.insert(stoi(argv[i]));
   }
+
   int max_bill = *(dollars_in.begin());
   int dollar_amount = stoi(argv[argc - 1]);
-  //sort(dollars_in.begin(), dollars_in.end(), Greater());
-
-  /*cout << endl << "dollars_in: ";
-  for(vector<int>::iterator it = dollars_in.begin(); 
-      it != dollars_in.end(); ++it) {
-    cout << *it << ' ';
-    }
-  cout << endl;*/
  
   vector<int> dollars;
   set<int, Greater>::iterator it = dollars_in.end();
@@ -151,6 +141,12 @@ bool check_for_solution(int change, const vector<int> &dollars,
   return true;
 }
 
+//WARNING: The way change_bills is set up right now, change can
+//only be made out of the denominations that we start with
+//This is problematic: we want change bills to include both
+//real-life denominations and whatever the user inputs
+//OR should they only be real life denominations? If so,
+//there has to be a limit on them. $1000 is probably a good limit
 void update_change_bills(int change, vector<int> &change_bills, 
                          const set<int, Greater> &dollars_in) {
   while(change != 0) { //update the change_bills 
@@ -186,73 +182,6 @@ void update_dollars(vector<int> &dollars, int &denomination,
     dollars.pop_back();
     denomination = *(++it);
   }
-  /*
-  if(denomination == 50) {
-    dollars.pop_back();
-    denomination = 20;
-    return;
-  }
-  else if(denomination == 20) {
-    dollars.pop_back();
-    denomination = 10;
-    return;
-  }
-  else if(denomination == 10) {
-    dollars.pop_back();
-    denomination = 5;
-    return;
-  }
-  else if(denomination == 5) {
-    dollars.pop_back();
-    denomination = 2;
-    return;
-  }
-  else if(denomination == 2) {
-    dollars.pop_back();
-    denomination = 1;
-    return;
-  }
-  else { //denomination == 1
-    for(unsigned i = 0; i < dollars.size(); ++i) {
-      if(dollars[i] == 1) {
-	dollars.erase(dollars.begin() + i, dollars.end());
-	//erase all of the 1s
-      }
-    }
-
-    /*cout << "dollars.back(): " << dollars.back() << endl;
-    cout << "dollars after 1s were supposed to be deleted: ";
-    for(unsigned i = 0; i < dollars.size(); ++i) {
-      cout << dollars[i] << " ";
-    }
-    cout << endl;
-   
-    if(dollars.back() == 50) {
-      dollars.pop_back();
-      denomination = 20;
-      return;
-    }
-    else if(dollars.back() == 20) {
-      dollars.pop_back();
-      denomination = 10;
-      return;
-    }
-    else if(dollars.back() == 10) {
-      dollars.pop_back();
-      denomination = 5;
-      return;
-    }
-    else if(dollars.back() == 5) {
-      dollars.pop_back();
-      denomination = 2;
-      return;
-    }
-    else { //at this point, the end can only be a 2 since all 1s are gone
-      dollars.pop_back();
-      denomination = 1;
-      return;
-    }
-  }*/
 }
 
 void print_solutions(vector<int> dollars, int max_bill, int dollar_amount, 
@@ -261,10 +190,10 @@ void print_solutions(vector<int> dollars, int max_bill, int dollar_amount,
   int total = 0;
   vector<int> change_bills;
   dollars.push_back(max_bill);
-  int denomination_to_add = max_bill; //first, add $50 bills to the vector
+  int denomination_to_add = max_bill;
   int change = 0;
   int test_count = 0;
-
+  cout << endl;
   while(dollars[0] != 1) {
     total = 0; //reset total
     change = 0; //reset change, may not be necessary
@@ -282,24 +211,23 @@ void print_solutions(vector<int> dollars, int max_bill, int dollar_amount,
 
     if(check_for_solution(change, dollars, change_bills)) {
       
-      cout << "solution: ";
+      cout << "Solution: ";
       for(unsigned i = 0; i < dollars.size(); ++i) {
         cout << dollars[i] << " ";
 	sum += dollars[i];
       }
-      cout << "sum: " << sum << endl;
+      cout << endl << "Sum of bills: " << sum << endl;
       sum = 0; //reset sum 
       ++test_count;
-      /*cout << "change_bills: ";
+      cout << "Change received: ";
       for(unsigned i = 0; i < change_bills.size(); ++i) {
         cout << change_bills[i] << " ";
       }
-      cout << endl;*/
+      cout << endl << endl;
     }
     update_dollars(dollars, denomination_to_add, dollars_in); 
     //pop the unwanted numbers off of the end
     change_bills.clear();
     //clear change_bills
   }
-  //cout << "test_count: " << test_count << endl;
 }
